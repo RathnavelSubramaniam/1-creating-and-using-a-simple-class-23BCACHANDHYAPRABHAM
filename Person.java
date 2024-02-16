6ac0410a37184911d07233bc9feb962c63e671e9
23BCACHANDHYAPRABHAM <144424467+23BCACHANDHYAPRABHAM@users.noreply.github.com>
2024-02-16 06:55:06 +0000

    s1

--- Person.java
+++ Person.java
@@ -13,7 +13,7 @@ public class Person {
         age = a;
     }
      String FullName() {
-        return firstName +  ""  + lastName;
+        return firstName +" "+ lastName;
     }
 
     public static void main(String[] args) {
